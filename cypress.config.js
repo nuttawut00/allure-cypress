3ed0d72cdcc505af998a914a19eaf68de0432794
nuttawut00 <nuttawutinpeng@gmail.com>
2025-02-17 14:11:53 +0700

    init allure report

--- cypress.config.js
+++ cypress.config.js
@@ -1,9 +1,12 @@
 const { defineConfig } = require("cypress");
+const { allureCypress } = require("allure-cypress/reporter");
 
 module.exports = defineConfig({
   e2e: {
     setupNodeEvents(on, config) {
-      // implement node event listeners here
+      allureCypress(on, config, {
+        resultsDir: "allure-results",
+      });
     },
   },
 });
